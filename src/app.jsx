d6906b0815232edf0e6a74a06b80821296bc8d89
Shashikumarezhilarasu <shashikumarezhilarasu@gmail.com>
2024-02-16 23:11:09 +0530

    everything fine butt

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './Components/Navbar';
 import NewsBoard from './Components/NewsBoard';
+
 const App = () => {
-  const[category,setCategory] =useState("genaral");
+  const [category, setCategory] = useState("general");
+
   return (
     <div>
-        <Navbar setCategory={setCategory}/>
-        <NewsBoard category={category}/>
+      <Navbar setCategory={setCategory} />
+      <NewsBoard category={category} />
     </div>
   );
 }
-export  default App;
-//ReactDOM.render(<App />, document.getElementById('root'));
-//
+
+export default App;
+
+// ReactDOM.render(<App />, document.getElementById('root'));
